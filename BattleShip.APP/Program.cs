adadb5bbe611409688636735dee3863d4b559bb4
msd-11 <msd0xB@pm.me>
2024-02-08 15:36:48 +0100

    Update frontend, begin UI/UX

--- BattleShip.APP/Program.cs
+++ BattleShip.APP/Program.cs
@@ -8,6 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddHttpClient();
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5023/") });
 
 await builder.Build().RunAsync();
