645eb9880c1b3bc4e7d7c276070da6aa4011ff1a
Matteo <matteo.michel@mines-ales.org>
2024-03-03 17:39:21 +0100

    feat: update fleet

--- BattleShip.API/Controller/FleetController.cs
+++ BattleShip.API/Controller/FleetController.cs
@@ -1,5 +1,6 @@
 using BattleShip.API.Service;
 using BattleShip.Models.DTO.Input;
+using BattleShip.Models.DTO.Output;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
@@ -21,12 +22,27 @@ public class FleetController : Controller
     
     [HttpPut]
     [Route("{warId}")]
-    public Ok UpdateFleet(FleetInput fleetInput, string warId)
+    public JsonHttpResult<WarOutput> UpdateFleet(FleetInput fleetInput, string warId)
     {
         var war = WarService.Wars[int.Parse(warId)];
         war.Seas[0] = FleetService.UpdateFleet(war.Seas[0], fleetInput);
-        
-        return TypedResults.Ok();
+
+        return TypedResults.Json(new WarOutput
+        {
+            Id = war.Id ?? 0,
+            gridSize = war.Seas[0].Size,
+            Ships = war.Seas[0].Pirate.Ships.Select(s =>
+                new ShipOutput
+                {
+                    Letter = s.Letter.ToString(),
+                    Positions = s.Positions.Select(p =>
+                        new ShipPositionOutput
+                        {
+                            X = p.X,
+                            Y = p.Y
+                        }).ToList()
+                }).ToList()
+        });
     }
     
 }
